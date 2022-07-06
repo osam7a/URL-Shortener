f66ffdd77a23866ca2926f1048c3abb77dfc93f6
osam7a <osamaalhinawi2008@gmail.com>
2022-07-06 19:20:19 +0400

    init

--- main.py
+++ main.py
@@ -4,16 +4,37 @@ from typing import Union
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 
-app = FastAPI()
+meta = [
+    {
+        "name": "New",
+        "description": "Add a new URL To our database"
+    },
+    {
+        "name": "Redirect",
+        "description": "Gets your link from our database and redirects you to it"
+    }
+]
+
+app = FastAPI(
+    title="Shoort URL Shortener",
+    description="Shorten any URL Using our REST API",
+    version="0.0.1 beta", 
+    docs_url="/",
+    openapi_tags=meta
+    )
 
 @app.get("/")
 async def main():
-    return {
-        "HELLOE EE": "E TST "
-    }
+    """
+    Documentation
+    """
+    return RedirectResponse(url="/docs", status_code=200)
 
-@app.get("/new")
+@app.get("/new", tags=["New"])
 async def new(link: str, custom_name: Union[str, None] = None):
+    """
+    Add a short link to our database
+    """
     if not custom_name:
         _ = shortuuid.ShortUUID(alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
         uuid = _.random(length=6)
@@ -24,8 +45,8 @@ async def new(link: str, custom_name: Union[str, None] = None):
             "short_link": "shoort.ml/" + uuid
         }
     else:
-        if len(custom_name) > 10:
-            raise HTTPException(status_code=400, detail="Custom name must be less than 10 characters")
+        if len(custom_name) > 20:
+            raise HTTPException(status_code=400, detail="Custom name must be less than 20 characters")
         if database.checkIfShortExists(custom_name):
             raise HTTPException(status_code=400, detail="Custom name already exists")
         database.addLink(link, custom_name)
@@ -35,12 +56,13 @@ async def new(link: str, custom_name: Union[str, None] = None):
             "short_link": "shoort.ml/" + custom_name
         }
 
-@app.get("/{short}")
+
+@app.get("/{short}", tags=["Redirect"])
 async def redir(short: str):
     if database.checkIfShortExists(short):
         link = database.getLink(short)
         if not link.startswith("https://") or not link.startswith("http://"):
             link = "http://" + link
-        return RedirectResponse(link)
+        return RedirectResponse(link, status_code=200)
     else:
         raise HTTPException(status_code=404, detail="We do not have such page!")
